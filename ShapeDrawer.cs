9d701fb95ede9ae233a06e0829ea959d37e5ba06
Ivan Eliseev <eliseev_ivan1964@mail.ru>
2022-05-16 16:22:52 +0300

    function IsImpimented changed

--- ShapeDrawer.cs
+++ ShapeDrawer.cs
@@ -10,17 +10,17 @@ namespace lab2
     static class ShapeDrawer
     {
 
-        private static bool Isimplemented(Type type, string name)
+        private static bool Isimplemented(Type type)
         {
             var interfaceType = type.GetInterfaces()
-                .FirstOrDefault(t => t.Name == name);
+                .FirstOrDefault(t => t.Name == "INonPolygon");
             return interfaceType != null;
         }
 
         public static void DrawFigure(IAbs figure, Graphics canvas, Point click)
         {
             Pen pen = new Pen(Color.Black, 3);
-            if (Isimplemented(figure.GetType(), "INonPolygon"))
+            if (Isimplemented(figure.GetType()))
             {
                 float[] arr = figure.FloatArr(click);
                 canvas.DrawEllipse(pen, arr[0], arr[1], arr[2], arr[3]);
